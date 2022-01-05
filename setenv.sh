8d027db832a91e32f2c2397bc67bba2914dea521
raikhovn <raikhovn@gmail.com>
2022-01-05 18:07:05 -0500

    initial review

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+# Set env vars for buildspec
+export IMAGE_REPO_NAME=raikhovn/httpserver
+export IMAGE_TAG=latest
+export IMAGE_REGISTRY=docker.io/raikhovn/httpserver
+export USER=raikhovn
+export PWD=Jamesbond1
